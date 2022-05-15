c95062111598d6b609ab248f87c12ebc6f2100ee
Sander Paniago <sanderppaniago@gmail.com>
2022-05-15 15:51:04 -0400

    print serial distance sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,38 @@
 #include <Arduino.h>
 
+const int TRIG = 3;
+const int ECHO = 2;
+const int distancia_obstaculo = 20;
+
+int sensor_morcego(int pinotrig, int pinoecho);
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(TRIG,OUTPUT);
+  pinMode(ECHO,INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int distancia = sensor_morcego(TRIG,ECHO);
+    if(distancia <= distancia_obstaculo){
+      Serial.print("Com obstaculo: ");
+      Serial.print(distancia);
+      Serial.println("cm");
+    }
+    else{
+      Serial.print("Sem obstaculo: ");
+      Serial.print(distancia);
+      Serial.println("cm");
+    }
+  delay(100);
+}
+
+int sensor_morcego(int pinotrig,int pinoecho){
+  digitalWrite(pinotrig,LOW);
+  delayMicroseconds(2);
+  digitalWrite(pinotrig,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(pinotrig,LOW);
+
+  return pulseIn(pinoecho,HIGH)/58;
 }
